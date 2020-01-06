0f322e0c82e8efd0ac97580bea8b3774348ee483
Mike <michael.schumacher@intertek.com>
2020-01-06 16:02:44 +0100

    Zwischenstand

--- src/Business/Applikation.ts
+++ src/Business/Applikation.ts
@@ -1,7 +1,7 @@
 import { Sportler } from './Sportler/Sportler';
 import { ISportler } from './Sportler/Sportler';
 import { Local } from 'protractor/built/driverProviders';
-// import { VirtualTimeScheduler } from 'rxjs';
+import { VirtualTimeScheduler } from 'rxjs';
 
 enum SpeicherOrtTyp {
     Lokal,
@@ -17,33 +17,55 @@ enum ProgrammTyp {
 
 enum StorageItemTyp {
     LetzterSpeicherOrt,
-    AktuellesProgramm
+    AktuellesProgramm,
+    AppData
 }
 
-
+class AppDataMap {
+    public LetzterSpeicherOrt: SpeicherOrtTyp;
+    public Sessions: Array<ISession> = [];
+    public AktuellesProgramm: ProgrammTyp;
+}
 
 class AppData {
     public LetzterSpeicherOrt: SpeicherOrtTyp;
     public AktuellesProgramm: ProgrammTyp;
+    public readonly Daten: AppDataMap = new AppDataMap();
+    readonly cAppData: string = "AppData";
 
     constructor() {
         // LetzterSpeicherOrt
-        let x = SpeicherOrtTyp[localStorage.getItem(StorageItemTyp.LetzterSpeicherOrt.toString())];
-        if (x === undefined) {
-            this.LetzterSpeicherOrt = SpeicherOrtTyp.Lokal;
-            localStorage.setItem(StorageItemTyp.LetzterSpeicherOrt.toString(), this.LetzterSpeicherOrt.toString());
-        } else {
-            this.LetzterSpeicherOrt = SpeicherOrtTyp[localStorage.getItem(StorageItemTyp.LetzterSpeicherOrt.toString())];
+        // let x = SpeicherOrtTyp[localStorage.getItem(StorageItemTyp.LetzterSpeicherOrt.toString())];
+        // if (x === undefined) {
+        //     this.LetzterSpeicherOrt = SpeicherOrtTyp.Lokal;
+        //     localStorage.setItem(StorageItemTyp.LetzterSpeicherOrt.toString(), this.LetzterSpeicherOrt.toString());
+        // } else {
+        //     this.LetzterSpeicherOrt = SpeicherOrtTyp[localStorage.getItem(StorageItemTyp.LetzterSpeicherOrt.toString())];
+        // }
+
+        // // AktuellesProgramm
+        // x = ProgrammTyp[localStorage.getItem(StorageItemTyp.AktuellesProgramm.toString())];
+        // if (x === undefined) {
+        //     this.AktuellesProgramm = ProgrammTyp.Ohne;
+        //     localStorage.setItem(StorageItemTyp.AktuellesProgramm.toString(), this.AktuellesProgramm.toString());
+        // } else {
+        //     this.AktuellesProgramm = ProgrammTyp[localStorage.getItem(StorageItemTyp.AktuellesProgramm.toString())];
+        // }
+
+        localStorage.clear();
+        // let s = JSON.stringify(this.Daten);
+        // localStorage.setItem(this.cAppData, s);
+        // let s1 = localStorage.getItem(this.cAppData);
+        // this.Daten = JSON.parse(s);
+        this.LadeDaten();
+    }
+
+    public LadeDaten(){
+        const d = localStorage.getItem(this.cAppData);
+        if(d != undefined){
+            this.Daten = JSON.parse(d);
         }
 
-        // AktuellesProgramm
-        x = ProgrammTyp[localStorage.getItem(StorageItemTyp.AktuellesProgramm.toString())];
-        if (x === undefined) {
-            this.AktuellesProgramm = ProgrammTyp.Ohne;
-            localStorage.setItem(StorageItemTyp.AktuellesProgramm.toString(), this.AktuellesProgramm.toString());
-        } else {
-            this.AktuellesProgramm = ProgrammTyp[localStorage.getItem(StorageItemTyp.AktuellesProgramm.toString())];
-        }
     }
 }
 
