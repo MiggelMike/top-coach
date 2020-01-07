fdcf5f62fe7f9f4d45a6048304a8b98bc7a0e815
mike schumacher <mike.schumacher@arcor.de>
2020-01-07 18:32:19 +0100

    Satz-Strukturen bearbeitet

--- src/Business/Konfiguration/Satz.ts
+++ src/Business/Konfiguration/Satz.ts
@@ -9,6 +9,7 @@ export interface ISatz {
     SessionID: number;
     UebungID: number;
     SatzTyp: SatzTyp;
+    NrInSession: number;
 }
 
 export interface IVorlageSatz extends ISatz {
@@ -18,26 +19,41 @@ export interface IVorlageSatz extends ISatz {
 
 export interface ITrainingsSatz extends ISatz {
     GewichtAusgefuehrt: number;
-    GewichtVorgabe: number;
+    WdhAusgefuehrt: number;
 }
 
-export abstract class VorlageSatz implements IVorlageSatz  {
-    ID: number;
-    SessionID: number;
-    UebungID: number;
-    SatzTyp: SatzTyp;
+export class Satz implements ISatz {
+    public ID: number;
+    public SessionID: number;
+    public UebungID: number;
+    public NrInSession: number;
+    public SatzTyp: SatzTyp;
+    constructor(aSessionID: number, aUebungID: number, aNrInSession: number, aSatzTyp: SatzTyp) {
+        this.SessionID = aSessionID;
+        this.UebungID = aUebungID;
+        this.SatzTyp = aSatzTyp;
+        this.NrInSession = aNrInSession;
+    }
+
+    // public static AddSatz(aSessionID: number, aUebungID: number, aSatzTyp: SatzTyp): ISatz {
+    //     const mSatz = new Satz();
+    //     mSatz.SessionID = aSessionID;
+    //     mSatz.UebungID = aUebungID;
+    //     mSatz.SatzTyp = aSatzTyp;
+    //     return mSatz;
+    // }
+}
+
+export abstract class VorlageSatz extends Satz implements IVorlageSatz  {
     WdhVorgabe: number;
     GewichtVorgabe: number;
 }
 
-export abstract class TrainingsSatz implements IVorlageSatz, ITrainingsSatz  {
-    ID: number;
-    SessionID: number;
-    UebungID: number;
-    SatzTyp: SatzTyp;
-    GewichtAusgefuehrt: number;
-    WdhVorgabe: number;
-    GewichtVorgabe: number;
+export abstract class TrainingsSatz extends VorlageSatz implements ITrainingsSatz  {
+    public GewichtAusgefuehrt: number;
+    public WdhAusgefuehrt: number;
+    public GewichtVorgabe: number;
+    public WdhVorgabe: number;
 }
 
 
--- src/Business/TrainingsProgramm/Gzclp.ts
+++ src/Business/TrainingsProgramm/Gzclp.ts
@@ -2,7 +2,7 @@ import { ITrainingsProgramm } from './TrainingsProgramm';
 import { TrainingsProgramm } from './TrainingsProgramm';
 import { IVorlageSession } from '../Session/Session';
 import { VorlageSession } from '../Session/Session';
-import { TrainingsSatz, VorlageSatz, ITrainingsSatz, IVorlageSatz } from '../Konfiguration/Satz';
+import { TrainingsSatz, VorlageSatz, ITrainingsSatz, IVorlageSatz, SatzTyp } from '../Konfiguration/Satz';
 
 export class GzclpProgramm extends TrainingsProgramm {
     constructor() {
@@ -18,9 +18,9 @@ export class GzclpProgramm extends TrainingsProgramm {
         switch (aTagNr) {
 
             case 1:
-                mEineSession.Saetze.push(new GzclpT1Cycle0VorlageSatz());
-                mEineSession.Saetze.push(new GzclpT1Cycle0VorlageSatz());
-                mEineSession.Saetze.push(new GzclpT1Cycle0VorlageSatz());
+                mEineSession.Saetze.push(new GzclpT1Cycle0VorlageSatz(mEineSession.ID, 0, 1, SatzTyp.Aufwaermen));
+                mEineSession.Saetze.push(new GzclpT1Cycle0VorlageSatz(mEineSession.ID, 0, 2, SatzTyp.Aufwaermen));
+                mEineSession.Saetze.push(new GzclpT1Cycle0VorlageSatz(mEineSession.ID, 0, 3, SatzTyp.Aufwaermen));
                 break;
 
             case 2:
@@ -43,65 +43,65 @@ export class GzclpVorlageSession extends VorlageSession {
 }
 
 export class GzclpT1Cycle0VorlageSatz extends VorlageSatz {
-    constructor() {
-        super();
+    constructor(aSessionID: number, aUebungID: number, aNrInSession: number, aSatzTyp: SatzTyp) {
+        super(aSessionID, aUebungID, aNrInSession, aSatzTyp);
         this.WdhVorgabe = 5;
     }
 }
 
-export class GzclpT1Cycle0TrainingsSatz extends TrainingsSatz {}
+export class GzclpT1Cycle0TrainingsSatz extends TrainingsSatz { }
 
 export class GzclpT1Cycle1VorlageSatz extends VorlageSatz {
-    constructor() {
-        super();
+    constructor(aSessionID: number, aUebungID: number, aNrInSession: number, aSatzTyp: SatzTyp) {
+        super(aSessionID, aUebungID, aNrInSession, aSatzTyp);
         this.WdhVorgabe = 3;
     }
 }
 
-export class GzclpT1Cycle1TrainingsSatz extends TrainingsSatz {}
+export class GzclpT1Cycle1TrainingsSatz extends TrainingsSatz { }
 
 export class GzclpT1Cycle2VorlageSatz extends VorlageSatz {
-    constructor() {
-        super();
+    constructor(aSessionID: number, aUebungID: number, aNrInSession: number, aSatzTyp: SatzTyp) {
+        super(aSessionID, aUebungID, aNrInSession, aSatzTyp);
         this.WdhVorgabe = 1;
     }
 }
 
-export class GzclpT1Cycle2TrainingsSatz extends TrainingsSatz {}
+export class GzclpT1Cycle2TrainingsSatz extends TrainingsSatz { }
 
 export class GzclpT2Cycle0VorlageSatz extends VorlageSatz {
-    constructor() {
-        super();
+    constructor(aSessionID: number, aUebungID: number, aNrInSession: number, aSatzTyp: SatzTyp) {
+        super(aSessionID, aUebungID, aNrInSession, aSatzTyp);
         this.WdhVorgabe = 10;
     }
 }
 
-export class GzclpT2Cycle0TrainingsSatz extends TrainingsSatz {}
+export class GzclpT2Cycle0TrainingsSatz extends TrainingsSatz { }
 
 export class GzclpT2Cycle1VorlageSatz extends VorlageSatz {
-    constructor() {
-        super();
+    constructor(aSessionID: number, aUebungID: number, aNrInSession: number, aSatzTyp: SatzTyp) {
+        super(aSessionID, aUebungID, aNrInSession, aSatzTyp);
         this.WdhVorgabe = 8;
     }
 }
 
-export class GzclpT2Cycle1TrainingsSatz extends TrainingsSatz {}
+export class GzclpT2Cycle1TrainingsSatz extends TrainingsSatz { }
 
 export class GzclpT2Cycle2VorlageSatz extends VorlageSatz {
-    constructor() {
-        super();
+    constructor(aSessionID: number, aUebungID: number, aNrInSession: number, aSatzTyp: SatzTyp) {
+        super(aSessionID, aUebungID, aNrInSession, aSatzTyp);
         this.WdhVorgabe = 6;
     }
 }
 
-export class GzclpT2Cycle2TrainingsSatz extends TrainingsSatz {}
+export class GzclpT2Cycle2TrainingsSatz extends TrainingsSatz { }
 
 export class GzclpT3VorlageSatz extends VorlageSatz {
-    constructor() {
-        super();
+    constructor(aSessionID: number, aUebungID: number, aNrInSession: number, aSatzTyp: SatzTyp) {
+        super(aSessionID, aUebungID, aNrInSession, aSatzTyp);
         this.WdhVorgabe = 15;
     }
 }
 
-export class GzclpT3TrainingsSatz extends TrainingsSatz {}
+export class GzclpT3TrainingsSatz extends TrainingsSatz { }
 
