71820adc58db2caa00a02148bd8844bad72fdeda
mike schumacher <mike.schumacher@arcor.de>
2020-01-07 18:32:19 +0100

    Satz-Strukturen bearbeitet

--- src/Business/Konfiguration/Satz.ts
+++ src/Business/Konfiguration/Satz.ts
@@ -18,26 +18,33 @@ export interface IVorlageSatz extends ISatz {
 
 export interface ITrainingsSatz extends ISatz {
     GewichtAusgefuehrt: number;
-    GewichtVorgabe: number;
+    WdhAusgefuehrt: number;
 }
 
-export abstract class VorlageSatz implements IVorlageSatz  {
-    ID: number;
-    SessionID: number;
-    UebungID: number;
-    SatzTyp: SatzTyp;
+export class Satz implements ISatz {
+    public ID: number;
+    public SessionID: number;
+    public UebungID: number;
+    public SatzTyp: SatzTyp;
+    public static AddSatz(aSessionID: number, aUebungID: number, aSatzTyp: SatzTyp): ISatz {
+        const mSatz = new Satz();
+        mSatz.SessionID = aSessionID;
+        mSatz.UebungID = aUebungID;
+        mSatz.SatzTyp = aSatzTyp;
+        return mSatz;
+    }
+}
+
+export abstract class VorlageSatz extends Satz implements IVorlageSatz  {
     WdhVorgabe: number;
     GewichtVorgabe: number;
 }
 
-export abstract class TrainingsSatz implements IVorlageSatz, ITrainingsSatz  {
-    ID: number;
-    SessionID: number;
-    UebungID: number;
-    SatzTyp: SatzTyp;
-    GewichtAusgefuehrt: number;
-    WdhVorgabe: number;
-    GewichtVorgabe: number;
+export abstract class TrainingsSatz extends VorlageSatz implements ITrainingsSatz  {
+    public GewichtAusgefuehrt: number;
+    public WdhAusgefuehrt: number;
+    public GewichtVorgabe: number;
+    public WdhVorgabe: number;
 }
 
 
