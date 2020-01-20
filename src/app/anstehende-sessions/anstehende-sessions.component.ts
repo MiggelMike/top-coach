7f9fed2bdf642aa7032ec37efa103f05924d2914
mike schumacher <mike.schumacher@arcor.de>
2020-01-20 18:37:51 +0100

    Anstehende Sessions eingebaut

---
+++ src/app/anstehende-sessions/anstehende-sessions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-anstehende-sessions',
+  templateUrl: './anstehende-sessions.component.html',
+  styleUrls: ['./anstehende-sessions.component.scss']
+})
+export class AnstehendeSessionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
