8da242195da8168d8ffda7dd3aec44e4c0eb923f
Mike <michael.schumacher@intertek.com>
2020-01-21 08:43:56 +0100

    Anstehende Sessions

--- src/app/services/trainings-programm-svc.service.ts
+++ src/app/services/trainings-programm-svc.service.ts
@@ -19,31 +19,40 @@ import { Observable, of, from } from 'rxjs';
 })
 export class TrainingsProgrammSvcService {
 
+    AnstehendeSessionObserver;
+
+    SessionListe: Array<Session> = [
+        {
+            ID: 1,
+            Name: 'aPara.Name',
+            Saetze: [],
+            Datum: new Date(),
+            DauerInSek: 1,
+            Typ: SessionKategorie.Konkret,
+            ProgrammTyp: ProgrammTyp.Custom
+        },
+        {
+            ID: 2,
+            Name: 'aPara.Name',
+            Saetze: [],
+            Datum: new Date(),
+            DauerInSek: 2,
+            Typ: SessionKategorie.Konkret,
+            ProgrammTyp: ProgrammTyp.Custom
+        }
+    ] ;
+
     constructor() { }
 
     public LadeAnstehendeSession(): Observable<Session[]> {
-        const mResult = new Array<Session>();
-        let mSession = new Session();
-        mSession.ID = 1;
-        mSession.Name = 'aPara.Name';
-        mSession.Saetze = [];
-        mSession.Datum = new Date();
-        mSession.DauerInSek = 1;
-        mSession.Typ = SessionKategorie.Konkret;
-        mSession.ProgrammTyp = ProgrammTyp.Custom;
+        const mResult = new Observable<Session[]>(
+            observer => {
+                this.AnstehendeSessionObserver = observer;
+                observer.next(this.SessionListe);
+            }
+        );
 
-        mSession = new Session();
-        mSession.ID = 2;
-        mSession.Name = 'aPara.Name2';
-        mSession.Saetze = [];
-        mSession.Datum = new Date();
-        mSession.DauerInSek = 2;
-        mSession.Typ = SessionKategorie.Konkret;
-        mSession.ProgrammTyp = ProgrammTyp.Custom;
-        mResult.push(mSession);
-        const moResult = new Observable<Session[]>();
-        //moResult
-        return mResult.map();
+        return mResult;
     }
 
 }
