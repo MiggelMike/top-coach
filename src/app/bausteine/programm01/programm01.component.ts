0a784285f00e0e233cb5393e574bc7a1d5a572f8
Mike <michael.schumacher@intertek.com>
2020-01-24 15:31:22 +0100

    Zwischenstand -> Programm wählen

--- src/app/bausteine/programm01/programm01.component.ts
+++ src/app/bausteine/programm01/programm01.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ITrainingsProgramm } from '../../../Business/TrainingsProgramm/TrainingsProgramm';
+import { GlobalData } from '../../services/global.service';
 
 
 @Component({
@@ -12,12 +13,21 @@ import { ITrainingsProgramm } from '../../../Business/TrainingsProgramm/Training
 
 export class Programm01Component implements OnInit {
     @Input() programm: ITrainingsProgramm;
-    @Input() programmLadeContext: number;
+    @Input() programmLadeContext: boolean | false;
+    @Input() programmtext: { value: null };
 
     constructor() {
     }
 
     ngOnInit() {
+    }
+
+    SelectThisWorkoutClick(): void {
+        if (GlobalData.AppData.AktuellesProgramm.Programm !== null) {
+
+        }
+        let x = 0;
+        x++;
     }
 
 }
