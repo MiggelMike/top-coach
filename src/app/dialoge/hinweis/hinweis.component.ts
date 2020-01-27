12a4576471a6858ebd4994f55075e84df4190540
mike schumacher <mike.schumacher@arcor.de>
2020-01-27 19:02:53 +0100

    Zwischenstand

--- src/app/dialoge/hinweis/hinweis.component.ts
+++ src/app/dialoge/hinweis/hinweis.component.ts
@@ -1,31 +1,24 @@
 import { Component, Inject } from '@angular/core';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
-
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 export interface DialogData {
     animal: string;
     name: string;
-  }
-
+}
 
 @Component({
-  selector: 'app-hinweis',
-  templateUrl: './hinweis.component.html',
-  styleUrls: ['./hinweis.component.scss']
+    selector: 'app-hinweis',
+    templateUrl: './hinweis.component.html',
+    styleUrls: ['./hinweis.component.scss']
 })
 
-export class HinweisComponent  {
-
+export class HinweisComponent {
 
     constructor(
         public dialogRef: MatDialogRef<HinweisComponent>,
         @Inject(MAT_DIALOG_DATA) public data: DialogData) { }
 
-        onNoClick(): void {
-            this.dialogRef.close();
-        }
-
-        onYesClick(): void {
-            this.dialogRef.close();
-        }
+    onOkClick(): void {
+        this.dialogRef.close();
+    }
 }
--- src/app/services/dialoge.service.ts
+++ src/app/services/dialoge.service.ts
@@ -1,27 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HinweisComponent } from '../dialoge/hinweis/hinweis.component';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
-
+import { MatDialogConfig, MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class DialogeService {
 
     name: string;
     animal: string;
-
     constructor(public fDialog: MatDialog) { }
 
     public Hinweis(aText: string): void {
-        const dialogRef = this.fDialog.open(HinweisComponent, {
-            width: '250px',
-            height: '250px',
-            data: {name: this.name, animal: this.animal}
-        });
+        const mDialogConfig = new MatDialogConfig();
+        mDialogConfig.width = '250px';
+        mDialogConfig.height = '450px';
+        mDialogConfig.disableClose = true;
+        mDialogConfig.autoFocus = true;
 
-        const x = dialogRef.afterClosed().subscribe(result => {
-            // console.log('The dialog was closed');
+        mDialogConfig.data = {
+            text: 'aText',
+            name: this.name,
+            animal: this.animal,
+            ok: false
+        };
+
+        const dialogRef = this.fDialog.open(HinweisComponent, mDialogConfig );
+        dialogRef.afterClosed().subscribe(result => {
             this.animal = result;
         });
     }
