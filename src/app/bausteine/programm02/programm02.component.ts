6913abc8ac5bb1aeda32cc74ae77af0976514d10
mike schumacher <mike.schumacher@arcor.de>
2020-09-09 11:29:51 +0200

    Buttons fuer Workout,Session und Satz eingebaut

--- src/app/bausteine/programm02/programm02.component.ts
+++ src/app/bausteine/programm02/programm02.component.ts
@@ -1,33 +1,67 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { ITrainingsProgramm } from "src/Business/TrainingsProgramm/TrainingsProgramm";
+import { Component, OnInit, Input } from "@angular/core";
 
-
-import { ISession } from 'src/Business/Session/Session';
+import { ISession } from "src/Business/Session/Session";
 
 @Component({
-  selector: 'app-programm02',
-  templateUrl: './programm02.component.html',
-  styleUrls: ['./programm02.component.scss']
+    selector: "app-programm02",
+    templateUrl: "./programm02.component.html",
+    styleUrls: ["./programm02.component.scss"],
 })
 export class Programm02Component implements OnInit {
-  @Input() SessionListe: Array<ISession> = [];
-  @Input() ShowButtons: Boolean = false;
+    @Input() SessionListe: Array<ISession> = [];
+    @Input() ShowButtons: Boolean = false;
+    @Input() Programm: ITrainingsProgramm = null;
 
-  //  @ViewChild('sessionAccordion') accordionSession: MatAccordion;
+    //  @ViewChild('sessionAccordion') accordionSession: MatAccordion;
 
-  constructor() { }
+    constructor() {}
 
+    ngOnInit() {}
 
-  ngOnInit() {
-  }
+    ngAfterViewInit() {
+        // if (this.matExpansionPanelQueryList) {
+        //   this.matExpansionPanelQueryList.changes.subscribe(
+        //     change => {
+        //       change.open();
+        //     }
+        //   );
+        // }
+    }
 
-  ngAfterViewInit() {
-    // if (this.matExpansionPanelQueryList) {
-    //   this.matExpansionPanelQueryList.changes.subscribe(
-    //     change => {
-    //       change.open();
-    //     }
-    //   );
-    // }
-  }
+    public AddDay() {
+        alert("AddDay");
+    }
 
+    public CopyDay() {
+        alert("CopyDay");
+    }
+
+    public DeleteDay() {
+        alert("DeleteDay");
+    }
+
+    public PasteDay() {
+        alert("PasteDay");
+    }
+
+    public SaveChanges() {
+        alert("SaveChanges");
+    }
+
+    public CancelChanges() {
+        alert("CancelChanges");
+    }
+
+    public AddExcercise() {
+        alert("Add Excercise");
+    }
+
+    public CopyExcercise() {
+        alert("Copy Excercise");
+    }
+
+    public PasteExcercise() {
+        alert("PasteExcercise");
+    }
 }
--- src/app/bausteine/programm03/programm03.component.ts
+++ src/app/bausteine/programm03/programm03.component.ts
@@ -22,39 +22,54 @@ export class Programm03Component implements OnInit {
 
     @Input() session: ISession;
 
-    ngOnInit() { }
-    
-    constructor(
-        private fDialogService: DialogeService
-    ) {}
+    ngOnInit() {}
+
+    constructor(private fDialogService: DialogeService) {}
 
     public SetWeight(value: number, satz: ISatz) {
         satz.GewichtVorgabe = value;
-    } 
+    }
 
-    public AddSet(
-        aUebung_Sess: IUebung_Sess,
-        aSatzTyp: string
-    ) {
-        let mSatz: ISatz;  
-        if(aSatzTyp === 'Training' )
-            mSatz = aUebung_Sess.NeuerSatz(SatzTyp.Training, LiftTyp.Custom, 0, 0, false);
+    public AddSet(aUebung_Sess: IUebung_Sess, aSatzTyp: string) {
+        let mSatz: ISatz;
+        if (aSatzTyp === "Training")
+            mSatz = aUebung_Sess.NeuerSatz(
+                SatzTyp.Training,
+                LiftTyp.Custom,
+                0,
+                0,
+                false
+            );
         aUebung_Sess.SatzListe.push(mSatz);
         return mSatz;
     }
 
-    public DeleteSet(aSatz: ISatz, aUebung_Sess: IUebung_Sess
-    ) {
+    public PasteSet(aUebung_Sess: IUebung_Sess) {
+        alert("PasteSet");
+    }
+    
+    public CopySet(aUebung_Sess: IUebung_Sess, aSatzTyp: string) {
+        alert("CopySet");
+    }    
+
+    public DeleteExercise(aUebung_Sess: IUebung_Sess) {
+        alert("DeleteExercise");
+    }  
+    
+    public CopyExcercise(aUebung_Sess: IUebung_Sess) {
+        alert("CopyExcercise");
+    }  
+
+    public DeleteSet(aSatz: ISatz, aUebung_Sess: IUebung_Sess) {
         const mDialogData = new DialogData();
-        mDialogData.textZeilen.push('Delete set?');
+        mDialogData.textZeilen.push("Delete set?");
         mDialogData.OkFn = () => {
             const index: number = aUebung_Sess.SatzListe.indexOf(aSatz);
             if (index !== -1) {
                 aUebung_Sess.SatzListe.splice(index, 1);
-            }  
+            }
         };
 
         this.fDialogService.JaNein(mDialogData);
-
     }
 }
