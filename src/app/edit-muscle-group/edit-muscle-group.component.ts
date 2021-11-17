ec6e9911d4916a8ab41d10e2cca1f532194af39f
mike schumacher <mike.schumacher@arcor.de>
2021-11-17 13:11:09 +0100

    ZS

--- src/app/edit-muscle-group/edit-muscle-group.component.ts
+++ src/app/edit-muscle-group/edit-muscle-group.component.ts
@@ -1,11 +1,10 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { MuscleGroup } from 'src/Business/MuscleGroup/MuscleGroup';
-import { Location } from '@angular/common'
+import { Location } from '@angular/common';
 import { DialogeService } from '../services/dialoge.service';
 import { DialogData } from '../dialoge/hinweis/hinweis.component';
 import { DexieSvcService } from '../services/dexie-svc.service';
-
 
 @Component({
   selector: 'app-edit-muscle-group',
@@ -47,49 +46,49 @@ export class EditMuscleGroupComponent implements OnInit {
     this.NameField.nativeElement.focus();
   }
 
-  SaveChanges() {
-    const mTmpEditMuscleGroupComponent: EditMuscleGroupComponent = this
-      .ClickData as EditMuscleGroupComponent;
-    if (mTmpEditMuscleGroupComponent.Muskelgruppe.Name.trim() === '') {
-      const mDialogData = new DialogData();
-      mDialogData.textZeilen.push('Please enter a name!');
-      mTmpEditMuscleGroupComponent.fDialogService.Hinweis(mDialogData);
-    } else {
-      if (
-        (mTmpEditMuscleGroupComponent.Muskelgruppe.ID === undefined ||
-          mTmpEditMuscleGroupComponent.Muskelgruppe.ID <= 0) &&
-        mTmpEditMuscleGroupComponent.fDexieSvcService.FindMuskel(
-          mTmpEditMuscleGroupComponent.Muskelgruppe
-        )
-      ) {
-        const mDialogData = new DialogData();
-        mDialogData.textZeilen.push(
-          `There is already a muscle with name "${mTmpEditMuscleGroupComponent.Muskelgruppe.Name}"!`
-        );
-        mTmpEditMuscleGroupComponent.fDialogService.Hinweis(mDialogData);
-      } else {
-        mTmpEditMuscleGroupComponent.fDexieSvcService
-          .MuskelgruppeSpeichern(mTmpEditMuscleGroupComponent.Muskelgruppe)
-          .then((mID: number) => {
-            mTmpEditMuscleGroupComponent.Muskelgruppe.ID = mID;
-            mTmpEditMuscleGroupComponent.CmpMuskelgruppe =
-              mTmpEditMuscleGroupComponent.Muskelgruppe.Copy();
-            mTmpEditMuscleGroupComponent.fDexieSvcService.LadeMuskelGruppen();
-          });
-      }
-    }
-  }
+	SaveChanges() {
+		const mTmpEditMuscleGroupComponent: EditMuscleGroupComponent = this
+			.ClickData as EditMuscleGroupComponent;
+		if (mTmpEditMuscleGroupComponent.Muskelgruppe.Name.trim() === '') {
+			const mDialogData = new DialogData();
+			mDialogData.textZeilen.push('Please enter a name!');
+			mTmpEditMuscleGroupComponent.fDialogService.Hinweis(mDialogData);
+		} else {
+			if (
+				(mTmpEditMuscleGroupComponent.Muskelgruppe.ID === undefined ||
+					mTmpEditMuscleGroupComponent.Muskelgruppe.ID <= 0) &&
+				mTmpEditMuscleGroupComponent.fDexieSvcService.FindMuskel(
+					mTmpEditMuscleGroupComponent.Muskelgruppe
+				)
+			) {
+				const mDialogData = new DialogData();
+				mDialogData.textZeilen.push(
+					`There is already a muscle with name "${mTmpEditMuscleGroupComponent.Muskelgruppe.Name}"!`
+				);
+				mTmpEditMuscleGroupComponent.fDialogService.Hinweis(mDialogData);
+			} else {
+				mTmpEditMuscleGroupComponent.fDexieSvcService
+					.MuskelgruppeSpeichern(mTmpEditMuscleGroupComponent.Muskelgruppe)
+					.then((mID: number) => {
+						mTmpEditMuscleGroupComponent.Muskelgruppe.ID = mID;
+						mTmpEditMuscleGroupComponent.CmpMuskelgruppe =
+							mTmpEditMuscleGroupComponent.Muskelgruppe.Copy();
+						mTmpEditMuscleGroupComponent.fDexieSvcService.LadeMuskelGruppen();
+					});
+			}
+		}
+	}
 
-  CancelChanges() {
-    const mTmpEditMuscleGroupComponent: EditMuscleGroupComponent = this
-      .ClickData as EditMuscleGroupComponent;
-    const mDialogData = new DialogData();
-    mDialogData.textZeilen.push('Cancel unsaved changes?');
-    mDialogData.OkFn = (): void => {
-      mTmpEditMuscleGroupComponent.Muskelgruppe =
-        mTmpEditMuscleGroupComponent.CmpMuskelgruppe.Copy();
-    };
+	CancelChanges() {
+		const mTmpEditMuscleGroupComponent: EditMuscleGroupComponent = this
+			.ClickData as EditMuscleGroupComponent;
+		const mDialogData = new DialogData();
+		mDialogData.textZeilen.push('Cancel unsaved changes?');
+		mDialogData.OkFn = (): void => {
+			mTmpEditMuscleGroupComponent.Muskelgruppe =
+				mTmpEditMuscleGroupComponent.CmpMuskelgruppe.Copy();
+		};
 
-    mTmpEditMuscleGroupComponent.fDialogService.JaNein(mDialogData);
-  }
+		mTmpEditMuscleGroupComponent.fDialogService.JaNein(mDialogData);
+	}
 }
