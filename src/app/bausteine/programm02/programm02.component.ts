f89060c1940ace0a8e539fb9b8caf8fb7c99ae9d
mike schumacher <mike.schumacher@arcor.de>
2021-11-30 15:17:54 +0100

    ZS

--- src/app/bausteine/programm02/programm02.component.ts
+++ src/app/bausteine/programm02/programm02.component.ts
@@ -1,249 +1,230 @@
-import { ISession } from 'src/Business/Session/Session';
-import { ISessionDB } from './../../../Business/SessionDB';
-import { SessionStatus } from '../../../Business/SessionDB';
-import { UebungWaehlenData } from './../../uebung-waehlen/uebung-waehlen.component';
-import { UebungsKategorie02 } from './../../../Business/Uebung/Uebung';
-import { DexieSvcService } from './../../services/dexie-svc.service';
-import { Session } from './../../../Business/Session/Session';
+import { ISession } from "src/Business/Session/Session";
+import { ISessionDB } from "./../../../Business/SessionDB";
+import { SessionStatus } from "../../../Business/SessionDB";
+import { UebungWaehlenData } from "./../../uebung-waehlen/uebung-waehlen.component";
+import { UebungsKategorie02 } from "./../../../Business/Uebung/Uebung";
+import { DexieSvcService } from "./../../services/dexie-svc.service";
+import { Session } from "./../../../Business/Session/Session";
 import { ITrainingsProgramm } from "src/Business/TrainingsProgramm/TrainingsProgramm";
 import { Output, EventEmitter, Component, OnInit, Input, ViewChildren, QueryList } from "@angular/core";
-import { MatAccordion, MatExpansionPanel } from '@angular/material/expansion';
+import { MatAccordion, MatExpansionPanel } from "@angular/material/expansion";
 import { DialogeService } from "./../../services/dialoge.service";
 import { DialogData } from "./../../dialoge/hinweis/hinweis.component";
 import { GlobalService } from "src/app/services/global.service";
-import { of } from 'rxjs';
-import { Uebung, IUebung } from 'src/Business/Uebung/Uebung';
-import { UebungService } from 'src/app/services/uebung.service';
-import { Router, NavigationExtras } from '@angular/router';
-import { threadId } from 'worker_threads';
+import { of } from "rxjs";
+import { Uebung, IUebung } from "src/Business/Uebung/Uebung";
+import { UebungService } from "src/app/services/uebung.service";
+import { Router, NavigationExtras } from "@angular/router";
+import { threadId } from "worker_threads";
 
 @Component({
-    selector: "app-programm02",
-    templateUrl: "./programm02.component.html",
-    styleUrls: ["./programm02.component.scss"],
+	selector: "app-programm02",
+	templateUrl: "./programm02.component.html",
+	styleUrls: ["./programm02.component.scss"],
 })
 export class Programm02Component implements OnInit {
-    @Input() programm: ITrainingsProgramm = null;
-    @Output() ProgrammSavedEvent = new EventEmitter<ITrainingsProgramm>();
-    @Input() SessionListe: Array<ISession> = [];
-    @Input() showButtons: Boolean = false;
-    @Input() showSaveButtons: Boolean = false;
-    @Input() bearbeitbar: Boolean = false;
-    @Input() StartButtonVisible: boolean = false;
-    @Input() SessionPanelsExpanded: boolean = false;
-    @Input() showWarmUpCheckBox: Boolean = true;
-    @Input() showCoolDownCheckBox: Boolean = true;    
-    @ViewChildren("accSession") accSession: QueryList<MatAccordion>;
-    @ViewChildren("panSession") panUebung: QueryList<MatExpansionPanel>;
+	@Input() programm: ITrainingsProgramm = null;
+	@Output() ProgrammSavedEvent = new EventEmitter<ITrainingsProgramm>();
+	@Input() SessionListe: Array<ISession> = [];
+	@Input() showButtons: Boolean = false;
+	@Input() showSaveButtons: Boolean = false;
+	@Input() bearbeitbar: Boolean = false;
+	@Input() StartButtonVisible: boolean = false;
+	@Input() SessionPanelsExpanded: boolean = false;
+	@Input() showWarmUpCheckBox: Boolean = true;
+	@Input() showCoolDownCheckBox: Boolean = true;
+	@ViewChildren("accSession") accSession: QueryList<MatAccordion>;
+	@ViewChildren("panSession") panUebung: QueryList<MatExpansionPanel>;
 
-    private isExpanded: Boolean = true;
-    public ToggleButtonText: string;
+	private isExpanded: Boolean = true;
+	public ToggleButtonText: string;
+	public ClickData: Programm02Component;
 
-    public SessionPanelsExpanded1():Boolean {
-        if (this.SessionPanelsExpanded === true)
-            return true;
-        return false;
-    }
+	public SessionPanelsExpanded1(): Boolean {
+		if (this.SessionPanelsExpanded === true) return true;
+		return false;
+	}
 
-    public StartButtonText(aSess: ISession): string {
-        if (aSess.Kategorie02 === undefined)
-            aSess.Kategorie02 = SessionStatus.Wartet; 
-        
-        switch (aSess.Kategorie02) {
-            case SessionStatus.Wartet: return "Start"; 
-            case SessionStatus.Laueft:
-            case SessionStatus.Pause: return "Go ahead";
-            case SessionStatus.Fertig:
-            case SessionStatus.FertigTimeOut: return "Finished";
-            default: return "?";
-        }
-    }
+	public StartButtonText(aSess: ISession): string {
+		if (aSess.Kategorie02 === undefined) aSess.Kategorie02 = SessionStatus.Wartet;
 
-    constructor(
-        private fDialogService: DialogeService,
-        private fGlobalService: GlobalService,
-        private fUebungService: UebungService,
-        public fDbModule: DexieSvcService,
-        private router: Router
-    ) {
-    }
+		switch (aSess.Kategorie02) {
+			case SessionStatus.Wartet:
+				return "Start";
+			case SessionStatus.Laueft:
+			case SessionStatus.Pause:
+				return "Go ahead";
+			case SessionStatus.Fertig:
+			case SessionStatus.FertigTimeOut:
+				return "Finished";
+			default:
+				return "?";
+		}
+	}
 
-    ngAfterViewInit() {
-        this.panUebung.forEach( pan  => {
-            pan.expanded = this.SessionPanelsExpanded;
-        });
-    }
+	constructor(private fDialogService: DialogeService, private fGlobalService: GlobalService, private fUebungService: UebungService, public fDbModule: DexieSvcService, private router: Router) {}
 
-    ngOnInit() {}
+	ngAfterViewInit() {
+		this.panUebung.forEach((pan) => {
+			pan.expanded = this.SessionPanelsExpanded;
+		});
+	}
 
-     public CopySession(aSession: ISession) {
-    //     this.fGlobalService.SessionKopie = aSession.Copy();
-     }
+	ngOnInit() {}
 
-    public DeleteSession(aSession: ISession, aRowNum: number) {
-        const mDialogData = new DialogData();
-        mDialogData.textZeilen.push(
-            `Delete session #${aRowNum + 1} "${aSession.Name}" ?`
-        );
-        mDialogData.OkFn = (): void => {
-            const index: number = this.SessionListe.indexOf(aSession);
-            if (index !== -1) {
-                this.SessionListe.splice(index, 1);
-            }
+	public CopySession(aSession: ISession) {
+		//     this.fGlobalService.SessionKopie = aSession.Copy();
+	}
 
-            if (this.fGlobalService.Comp03PanelUebungObserver != null) {
-                //this.panUebung.expanded = false;
-                of(this.panUebung).subscribe(
-                    this.fGlobalService.Comp03PanelUebungObserver
-                );
-            }
-        };
+	public DeleteSession(aSession: ISession, aRowNum: number) {
+		const mDialogData = new DialogData();
+		mDialogData.textZeilen.push(`Delete session #${aRowNum + 1} "${aSession.Name}" ?`);
+		mDialogData.OkFn = (): void => {
+			const index: number = this.SessionListe.indexOf(aSession);
+			if (index !== -1) {
+				this.SessionListe.splice(index, 1);
+			}
 
-        this.fDialogService.JaNein(mDialogData);
-    }
+			if (this.fGlobalService.Comp03PanelUebungObserver != null) {
+				//this.panUebung.expanded = false;
+				of(this.panUebung).subscribe(this.fGlobalService.Comp03PanelUebungObserver);
+			}
+		};
 
-    // Wird an "AddExercise" als Parameter uebergeben
-    private SelectUebungDelegate(aUebungWaehlenData: UebungWaehlenData) {
-        aUebungWaehlenData.fUebungsListe.forEach((mUebung) => {
-            if (mUebung.Selected) {
-                aUebungWaehlenData.fSession.addUebung(
-                    Uebung.StaticKopiere(
-                        mUebung,
-                        UebungsKategorie02.Session
-                ))
-            }
-        });
-        aUebungWaehlenData.fMatDialog.close();
-    }
+		this.fDialogService.JaNein(mDialogData);
+	}
 
-    public AddExercise(aSession: ISession) {
-        if (this.fDbModule.StammUebungsListe.length === 0)
-            this.fDbModule.LadeStammUebungen();
-        else
-            this.fUebungService.UebungWaehlen(
-                this.fDbModule.StammUebungsListe,
-                aSession as Session,
-                this.SelectUebungDelegate
-            );
-    }
+	// Wird an "AddExercise" als Parameter uebergeben
+	private SelectUebungDelegate(aUebungWaehlenData: UebungWaehlenData) {
+		aUebungWaehlenData.fUebungsListe.forEach((mUebung) => {
+			if (mUebung.Selected) {
+				aUebungWaehlenData.fSession.addUebung(Uebung.StaticKopiere(mUebung, UebungsKategorie02.Session));
+			}
+		});
+		aUebungWaehlenData.fMatDialog.close();
+	}
 
-    public PasteExcercise(aSession: ISession) {
-        if (this.fGlobalService.SessUebungKopie === null) {
-            const mDialoData = new DialogData();
-            mDialoData.textZeilen.push("No data to paste!");
-            this.fDialogService.Hinweis(mDialoData);
-            return;
-        }
+	public AddExercise(aSession: ISession) {
+		if (this.fDbModule.StammUebungsListe.length === 0) this.fDbModule.LadeStammUebungen();
+		else this.fUebungService.UebungWaehlen(this.fDbModule.StammUebungsListe, aSession as Session, this.SelectUebungDelegate);
+	}
 
-        const mSessUebung: Uebung = this.fGlobalService.SessUebungKopie.Copy();
-        mSessUebung.SessionID = aSession.ID;
-        aSession.UebungsListe.push(mSessUebung);
-    }
+	public PasteExcercise(aSession: ISession) {
+		if (this.fGlobalService.SessUebungKopie === null) {
+			const mDialoData = new DialogData();
+			mDialoData.textZeilen.push("No data to paste!");
+			this.fDialogService.Hinweis(mDialoData);
+			return;
+		}
 
-    public toggleSessions(): void {
-        if (!this.accSession) return;
+		const mSessUebung: Uebung = this.fGlobalService.SessUebungKopie.Copy();
+		mSessUebung.SessionID = aSession.ID;
+		aSession.UebungsListe.push(mSessUebung);
+	}
 
-        if (this.isExpanded) {
-            this.accSession.forEach((acc) => acc.closeAll());
-            this.isExpanded = false;
-            this.ToggleButtonText = "Open all Sessions";
-        } else {
-            this.accSession.forEach((acc) => acc.openAll());
-            this.isExpanded = true;
-            this.ToggleButtonText = "Close all Sessions";
-        }
-    }
+	public toggleSessions(): void {
+		if (!this.accSession) return;
 
-    public accCheckSessionPanels() {
-        if (!this.panUebung) return;
+		if (this.isExpanded) {
+			this.accSession.forEach((acc) => acc.closeAll());
+			this.isExpanded = false;
+			this.ToggleButtonText = "Open all Sessions";
+		} else {
+			this.accSession.forEach((acc) => acc.openAll());
+			this.isExpanded = true;
+			this.ToggleButtonText = "Close all Sessions";
+		}
+	}
 
-        let mAllClosed = true;
+	public accCheckSessionPanels() {
+		if (!this.panUebung) return;
 
-        const mPanUebungListe = this.panUebung.toArray();
-        for (let index = 0; index < mPanUebungListe.length; index++) {
-            if (mPanUebungListe[index].expanded) {
-                mAllClosed = false;
-                break;
-            }
-        }
+		let mAllClosed = true;
 
-        if (mAllClosed) {
-            this.isExpanded = false;
-            this.ToggleButtonText = "Open all sessions";
-        } else {
-            this.isExpanded = true;
-            this.ToggleButtonText = "Close all sessions";
-        }
-    }
+		const mPanUebungListe = this.panUebung.toArray();
+		for (let index = 0; index < mPanUebungListe.length; index++) {
+			if (mPanUebungListe[index].expanded) {
+				mAllClosed = false;
+				break;
+			}
+		}
 
-    public AddSession() {
-        const mDate = new Date();
-        const mSession: Session = new Session();
-        mSession.Name = `Session ${mDate.toLocaleString()}`;
-        mSession.Datum = new Date();
-        mSession.DauerInSek = 0;
-        mSession.SessionNr = this.SessionListe.length + 1;
-        mSession.Kategorie01 = SessionStatus.Bearbeitbar;
-        //mSession.FK_Programm = this.programm.ID;
-        this.SessionListe.push(mSession);
-    }
+		if (mAllClosed) {
+			this.isExpanded = false;
+			this.ToggleButtonText = "Open all sessions";
+		} else {
+			this.isExpanded = true;
+			this.ToggleButtonText = "Close all sessions";
+		}
+	}
 
-    public PasteSession() {
-        if (this.fGlobalService.SessionKopie === null) {
-            const mDialoData = new DialogData();
-            mDialoData.textZeilen.push("No data to paste!");
-            this.fDialogService.Hinweis(mDialoData);
-            return;
-        }
+	public AddSession() {
+		const mDate = new Date();
+		const mSession: Session = new Session();
+		mSession.Name = `Session ${mDate.toLocaleString()}`;
+		mSession.Datum = new Date();
+		mSession.DauerInSek = 0;
+		mSession.SessionNr = this.SessionListe.length + 1;
+		mSession.Kategorie01 = SessionStatus.Bearbeitbar;
+		//mSession.FK_Programm = this.programm.ID;
+		this.SessionListe.push(mSession);
+	}
 
-        const mSession: ISessionDB = this.fGlobalService.SessionKopie.Copy();
-        //mSession.FK_Programm = this.programmID;
-        this.SessionListe.push(mSession as Session);
-    }
+	public PasteSession() {
+		if (this.fGlobalService.SessionKopie === null) {
+			const mDialoData = new DialogData();
+			mDialoData.textZeilen.push("No data to paste!");
+			this.fDialogService.Hinweis(mDialoData);
+			return;
+		}
 
-    public startSession(aEvent: Event, aSession: ISession) {
-        aEvent.stopPropagation();
+		const mSession: ISessionDB = this.fGlobalService.SessionKopie.Copy();
+		//mSession.FK_Programm = this.programmID;
+		this.SessionListe.push(mSession as Session);
+	}
 
-        if ((aSession.Kategorie02 === SessionStatus.Fertig)||(aSession.Kategorie02 === SessionStatus.FertigTimeOut) ) return;
-        
+	public startSession(aEvent: Event, aSession: ISession) {
+		aEvent.stopPropagation();
 
-        switch (aSession.Kategorie02) {
-            case SessionStatus.Wartet:
-                aSession.GestartedWann = new Date();
-                aSession.Kategorie02 = SessionStatus.Laueft; 
-                break;
-            
-            case SessionStatus.Pause: aSession.StarteDauerTimer(); break;
-            // case SessionStatus.Laueft: aSession.StartePauseTimer(); break;
-        }
+		if (aSession.Kategorie02 === SessionStatus.Fertig || aSession.Kategorie02 === SessionStatus.FertigTimeOut) return;
 
-        // aSession.GestartedWann = new Date();
-        // const navigationExtras: NavigationExtras = {
-        //     state: {
-        //       sess: aSession
-        //     }
-        //   };
-        this.router.navigate(['sessionFormComponent'], { state: { sess: aSession } } );
-    }
+		switch (aSession.Kategorie02) {
+			case SessionStatus.Wartet:
+				aSession.GestartedWann = new Date();
+				aSession.Kategorie02 = SessionStatus.Laueft;
+				break;
 
-    public SaveChanges(para: any) {
-        para.DbModule.ProgrammSpeichern(para.programm);
-        if (para.ProgrammSavedEvent != undefined)
-            para.ProgrammSavedEvent.emit(para.programm);
-    }
+			case SessionStatus.Pause:
+				aSession.StarteDauerTimer();
+				break;
+			// case SessionStatus.Laueft: aSession.StartePauseTimer(); break;
+		}
 
-    public CancelChanges() {
-        
-    }
+		// aSession.GestartedWann = new Date();
+		// const navigationExtras: NavigationExtras = {
+		//     state: {
+		//       sess: aSession
+		//     }
+		//   };
+		this.router.navigate(["sessionFormComponent"], { state: { sess: aSession } });
+	}
 
-    private CheckStatus() {
-        for (let index = 0; index < this.SessionListe.length; index++) {
-            this.StartButtonText(this.SessionListe[index]);
-        }
-    }
+	public SaveChanges() {
+		this.ClickData.fDbModule.ProgrammSpeichern(this.ClickData.programm);
+        if (this.ClickData.ProgrammSavedEvent !== undefined)
+            this.ClickData.ProgrammSavedEvent.emit(this.ClickData.programm);
+	}
 
-    ngDoCheck() {
-        // this.CheckStatus();
-        this.accCheckSessionPanels();
-    }
+	public CancelChanges() {}
+
+	private CheckStatus() {
+		for (let index = 0; index < this.SessionListe.length; index++) {
+			this.StartButtonText(this.SessionListe[index]);
+		}
+	}
+
+	ngDoCheck() {
+		// this.CheckStatus();
+		this.accCheckSessionPanels();
+	}
 }
