eb4f6787f84e3760aa7c91bbecd22c039d168fdb
mike schumacher <mike.schumacher@arcor.de>
2023-11-01 16:09:14 +0100

    zs

--- src/app/Example/Example.ts
+++ src/app/Example/Example.ts
@@ -1,22 +1,41 @@
+import { Session } from './../../Business/Session/Session';
 import { ITrainingsProgramm } from '../../Business/TrainingsProgramm/TrainingsProgramm';
-import { ProgrammParaDB } from '../services/dexie-svc.service';
-import { LadeAktuellesProgramm } from './../app-routing.module';
-import { DexieSvcService } from 'src/app/services/dexie-svc.service';
+import { DexieSvcService, ProgrammParaDB, SatzParaDB, SessionParaDB, UebungParaDB } from '../services/dexie-svc.service';
+
+
 export class Example {
     constructor(private fDbModul: DexieSvcService) {
         
     }
 
-    public MakeExample() {
+    public MakeExample(aProgrammName: string) {
         // const BisDatum: Date;
         const mProgrammPara: ProgrammParaDB = new ProgrammParaDB();
+        mProgrammPara.SessionBeachten = true;
+        mProgrammPara.SessionParaDB = new SessionParaDB();
+        mProgrammPara.SessionParaDB.UebungenBeachten = true; 
+        mProgrammPara.SessionParaDB.UebungParaDB = new UebungParaDB();
+        mProgrammPara.SessionParaDB.UebungParaDB.SaetzeBeachten = true;
+        mProgrammPara.SessionParaDB.UebungParaDB.SatzParaDB = new SatzParaDB();
+
+        mProgrammPara.WhereClause = "Name";
+        mProgrammPara.anyOf = () => {
+            return aProgrammName as any;
+        };
+
         // this.fDbModul.LadeProgrammeEx(mProgrammPara)
-        //     .then(() => 
-        //     const mMaxWochen: number = 8;
-        //     for (let mWoche = 0; mWoche < mMaxWochen; mWoche++) {
+        //     .then((aProgrammListe: Array<ITrainingsProgramm>) => {
+        //         const mMaxWochen: number = 8;
+        //         let mHeute: Date = new Date(); 
                 
-            
-        //     }
+
+        //         for (let mProgrammIndex = 0; mProgrammIndex < aProgrammListe.length; mProgrammIndex++) {
+        //             const mProgramm: ITrainingsProgramm = aProgrammListe[mProgrammIndex];
+        //             for (let mWoche = 0; mWoche < mMaxWochen; mWoche++) {
+                    
+        //             }//for
+        //             break;
+        //         }//for
         // }
     }
 }
