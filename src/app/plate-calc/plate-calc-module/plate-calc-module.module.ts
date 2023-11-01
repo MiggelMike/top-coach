c7d691ca074698996dae5e6ec4f1a91a28f0512d
mike schumacher <mike.schumacher@arcor.de>
2023-11-01 14:12:04 +0100

    zs

--- src/app/plate-calc/plate-calc-module/plate-calc-module.module.ts
+++ src/app/plate-calc/plate-calc-module/plate-calc-module.module.ts
@@ -1,7 +1,51 @@
 import { PlateCalcComponent } from '../plate-calc.component';
 import { NgModule, NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { DecimalPipe } from '@angular/common';
+import { CommonModule, DatePipe, DecimalPipe } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRippleModule, MatNativeDateModule } from '@angular/material/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgxMaskDirective, NgxMaskPipe } from 'ngx-mask';
+import {CdkDrag} from '@angular/cdk/drag-drop';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTreeModule } from '@angular/material/tree';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { Programm01Module } from 'src/app/bausteine/programm01/programm01Md/programm01.module';
+import { Programm02Module } from 'src/app/bausteine/programm02/programm02Md/programm02.module';
+import { Programm03Module } from 'src/app/bausteine/programm03/programm03Md/programm03.module';
+import { ToolBarModule } from 'src/app/bausteine/toolbar/tool-bar-module/tool-bar-module.module';
 // import { MatExpansionModule } from "@angular/material/expansion";
 // import { DatePipe } from "@angular/common";
 
@@ -10,6 +54,76 @@ import { DecimalPipe } from '@angular/common';
   exports: [
     PlateCalcComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
-  imports: [FormsModule,DecimalPipe]
+  imports: [
+    CdkDrag,
+    NgxMaskDirective,
+    NgxMaskPipe,
+    CommonModule,
+    FormsModule,
+    Programm01Module,
+    Programm02Module,
+    Programm03Module,
+    ToolBarModule,
+    FlexLayoutModule,
+    MatExpansionModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatRippleModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatSelectModule,
+    MatTabsModule,
+    MatNativeDateModule,
+    MatCardModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatIconModule,
+    NgxChartsModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatTooltipModule,
+    MatTreeModule,
+
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule,
+    DatePipe
+  ]
 })
 export class PlateCalcModule { }
