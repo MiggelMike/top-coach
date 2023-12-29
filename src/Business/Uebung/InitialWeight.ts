75a6831380648866567497fe0d31f092f810be8a
mike schumacher <mike.schumacher@arcor.de>
2023-12-29 12:27:23 +0100

    Initial-Weight

--- src/Business/Uebung/InitialWeight.ts
+++ src/Business/Uebung/InitialWeight.ts
@@ -1,5 +1,13 @@
+import { formatNumber } from "@angular/common";
+import { cWeightFormat } from "src/app/services/dexie-svc.service";
+
 export class InitialWeight {
     UebungID: number = 0;
     Name: string;
     Weight: number = 0;
+    get WeightText(): string{
+        return formatNumber(this.Weight,'en-US',cWeightFormat);
+    }
 }
+
+
